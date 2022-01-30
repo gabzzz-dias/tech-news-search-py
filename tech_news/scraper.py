581b9874b752873398a73dba9ebfc4c9eada8193
gabzzz-dias <gabriel.menezes.dias@hotmail.com>
2022-01-30 15:53:41 -0300

    req5

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,7 +1,7 @@
 import requests
 import time
 from parsel import Selector
-# from tech_news.database import create_news
+from tech_news.database import create_news
 
 
 def fetch(url):
@@ -31,11 +31,59 @@ def scrape_next_page_link(html_content):
     return sel.css(".tec--btn--lg::attr(href)").get()
 
 
-# Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    sel = Selector(html_content)
+
+    url = sel.css("link[rel=canonical]::attr(href)").get()
+    title = sel.css(".tec--article__header__title::text").get()
+    date = sel.css("time::attr(datetime)").get()
+    writer = sel.css(
+        ".z--font-bold").css("*::text").get().strip() or ""
+    comments = sel.css(".tec--btn::attr(data-count)").get()
+    summary = "".join(sel.css(
+        ".tec--article__body > p:nth-child(1) ::text"
+    ).getall())
+    sources = sel.css(
+        ".z--mb-16 .tec--badge::text"
+    ).getall()
+    categories = sel.css(
+        ".tec--badge--primary::text"
+    ).getall()
+
+    try:
+        shares = sel.css(
+            ".tec--toolbar__item::text"
+        ).get().strip().split(" ")[0]
+    except AttributeError:
+        shares = 0
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": date,
+        "writer": writer,
+        "shares_count": int(shares),
+        "comments_count": int(comments),
+        "summary": summary,
+        "sources": [x.strip() for x in sources],
+        "categories": [y.strip() for y in categories],
+    }
 
 
-# Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    url = fetch("https://www.tecmundo.com.br/novidades")
+    data = scrape_novidades(url)
+
+    if len(data) < amount:
+        next_page = fetch(scrape_next_page_link(url))
+        data.extend(scrape_novidades(next_page))
+    result = []
+
+    for x in data:
+        new = scrape_noticia(fetch(x))
+
+        if len(result) < amount:
+            result.append(new)
+    create_news(result)
+
+    return result
