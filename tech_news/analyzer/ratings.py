897082ccad1f752cd1e719f4c00911de08656b23
gabzzz-dias <gabriel.menezes.dias@hotmail.com>
2022-01-30 18:10:39 -0300

    req10-11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,34 @@
-# Requisito 10
+from tech_news.database import find_news
+from tech_news.database import db
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    data = find_news()
+    result = []
+
+    for x in data:
+        x["soma"] = x["shares_count"] + x["comments_count"]
+
+    data.sort(key=lambda x: x["soma"], reverse=True)
+    news = data[:5]
+
+    for x in news:
+        result.append((x["title"], x["url"]))
+
+    return result
 
 
-# Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    result = []
+    data = db.news.aggregate(
+        [
+            {"$unwind": "$categories"},
+            {"$group": {"_id": "$categories", "total": {"$sum": 1}}},
+            {"$sort": {"_id": 1}},
+        ]
+    )
+
+    for x in data:
+        result.append(x["_id"])
+
+    return result[:5]
